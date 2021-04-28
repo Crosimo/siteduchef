b0d5704b88f75648e235aa3f42dbaf82190949a9
Crosimo <jean.deborsu@hotmail.com>
2021-04-28 11:28:04 +0200

    ouiiiii

--- public/java/main.js
+++ public/java/main.js
@@ -1,6 +1,28 @@
-const yes = document.querySelector('.clicker');
-const no = document.querySelector('.attente')
-yes.addEventListener('click', function () {
-    no.classList.add('hidden');
+const yes = document.getElementsByClassName("clicker");
+console.log(yes.length);
+const no = document.querySelector('.attente');
+for (let i = 0; i <= yes.length; i++)
+{
+    yes[i].addEventListener('click', function () {
+        no.classList.add('hidden');
+        console.log('heyo');
+        
+    })
+};
+
+
+// yes[1].addEventListener('click', function () {
+//     no.classList.add('hidden');
+//     console.log('heyo');
     
-})
+// })
+// yes[2].addEventListener('click', function () {
+//     no.classList.add('hidden');
+//     console.log('heyo');
+    
+// })
+// yes[3].addEventListener('click', function () {
+//     no.classList.add('hidden');
+//     console.log('heyo');
+    
+// })
