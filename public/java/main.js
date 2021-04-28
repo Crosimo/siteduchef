abbf6ad4bee1a6166368b3dd0feb8a4ce03907ee
Crosimo <jean.deborsu@hotmail.com>
2021-04-28 10:01:21 +0200

    encore?

--- public/java/main.js
+++ public/java/main.js
@@ -1,23 +1,6 @@
-
-const btn1 = document.querySelector('.button1')
-const btn2 = document.querySelector('.button2')
-const btn3 = document.querySelector('.button3')
-const btn4 = document.querySelector('.button4')
-
-
-btn1.addEventListener('click', function () {
-    btn1.classList.add('hidden');
-    btn2.classList.remove('hidden');
-})
-btn2.addEventListener('click', function () {
-    document.querySelector('.button1').classList.add('hidden');
-    document.querySelector('.button2').classList.remove('hidden');
-})
-btn3.addEventListener('click', function () {
-    document.querySelectorAll('.button').classList.add('hidden');
-    document.querySelector('.button3').classList.remove('hidden');
-})
-btn4.addEventListener('click', function () {
-    document.querySelectorAll('.button1').classList.add('hidden');
-    document.querySelector('.button4').classList.remove('hidden');
+const yes = document.querySelector('.clicker');
+const no = document.querySelector('.attente')
+yes.addEventListener('click', function () {
+    no.classList.add('hidden');
+    
 })
